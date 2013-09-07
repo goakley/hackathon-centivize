6c0ba69290dc3c650dbf02dc6633c826b19dfe67
Glen Oakley <goakley123@gmail.com>
2013-09-06 22:34:20 -0400

    Added task getting code

--- server/server.js
+++ server/server.js
@@ -1,1 +1,27 @@
-var express;
+var redis = require('redis').createClient();
+
+
+
+function getTasks(uid, callback) {
+    redis.smembers("user:" + uid + ":taskids", function(err, res) {
+        if (err) {
+            callback(undefiend);
+            return;
+        }
+        var tasks = [];
+        for (var i = 0; i < res.length; i++) {
+            redis.hgetall("tasks:" + res[i], function(err, task) {
+                tasks.push(err ? undefined : task);
+                if (tasks.length === res.length) {
+                    callback(tasks);
+                    return;
+                }
+            });
+        }
+    });
+}
+
+function addTask(uid, task, callbacks) {
+}
+
+getTasks(0, function(response) { console.log(response); });
