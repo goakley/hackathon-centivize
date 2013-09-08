13c9837d9e34861d2b973e457cb626d3029f4720
Glen Oakley <goakley123@gmail.com>
2013-09-08 00:21:33 -0400

    I am the onceler

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.get(url.parse(CONFIG.DWOLLA.AUTH_CALLBACK).pathname, function(req, res) {
             redirect_uri: CONFIG.DWOLLA.AUTH_CALLBACK,
             code: req.query.code
         }
-    }).on('complete', function(data) {
+    }).once('complete', function(data) {
         console.log("REQUEST COMPLETE, STORING");
         redis.set("user:" + req.session.email + ":dwollatoken", data.access_token, function(err, asdf) {
             res.redirect('/');
